28f22126356a28cabb5916bd503bfaae16d9e349
Quentin ROBERT <zinatiix@gmail.com>
2021-05-23 20:35:33 +0200

    Added a version manager

--- commands/version.js
+++ commands/version.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "version",
     description: "A command that allows you to see the bot's version.",
     execute(m, args) {
-        m.reply("We're currently on version **" + package.version + "**");
+        m.reply("We're currently on version **" + pack.version + "**");
         return;
     }
 }
