6ccc1c0c9331de149f60a37141f789d9d8c77852
Quentin ROBERT <zinatiix@gmail.com>
2021-05-28 12:27:49 +0200

    Added help indicator

--- commands/bet.js
+++ commands/bet.js
@@ -17,7 +17,7 @@ module.exports = {
                     if(bets[userId] != null) return m.reply("You have already bet.");
                     if(betState["closed"]) return m.reply('Bets are not opened.');
                     m.reply("Your bet has been placed on the car N°" + carNumber);
-                    m.reply("The odd for this driver is " + odd[carNumber] + " so your gain will be (*rounded up to the next integer*) : " + Math.ceil(price*odd[carNumber]) + " <:f1coin:834897400610029568>");
+                    m.reply("The odd for this driver is " + odd[carNumber] + " so your gain will be (*rounded up to the next integer*) "+ price + "*" + odd[carNumber] + " : " + Math.ceil(price*odd[carNumber]) + " <:f1coin:834897400610029568>");
                     let bet = [carNumber, price];
                     bets[userId] = bet;
                     setBalance(userId, getBalance(userId) - price);
@@ -32,7 +32,7 @@ module.exports = {
     getHelp(m) {
         let embed = new MessageEmbed()
             .setTitle("Bet Command")
-            .addField("Description: ", "This command allows you place your bet on the driver you think will win the next race.", false)
+            .addField("Description: ", "This command allows you place your bet on the driver you think will win the next race. Your gain will be calculated based on the following formula : what you pay * the odd of the driver. Since we want to make sure you never have some bizarre balance, we are rounding up this gain (to the next integer because we are nice people). For instance, if you bet 1 <:f1coin:834897400610029568> on Lewis HAMILTON, who's odd is at " + odd["44"] + " your gain would be " + 1*odd["44"] + " <:f1coin:834897400610029568> but will instead be " + Math.ceil(1*odd["44"]) + " <:f1coin:834897400610029568>.", false)
             .addField("Usage: ", "$bet <carNumber> <price>")
             .setFooter("Anybody can use this command.")
             .setColor("GREEN");
